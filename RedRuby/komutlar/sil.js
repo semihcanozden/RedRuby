1cb5630406db67f113af70784acbbc2505d77157
semihcanozden <71717676+semihcanozden@users.noreply.github.com>
2021-08-15 18:27:50 +0300

    Add files via upload

---
+++ RedRuby/komutlar/sil.js
@@ -0,0 +1,18 @@
+module.exports = {
+  kod: "delete",
+  async run (client, message, args)
+  {
+    if (isNaN(args)) return message.reply('Lütfen bir sayı giriniz.')
+    if(args< 2 || args >100) return message.reply('Lütfen 2-100 arasında bir sayı giriniz.')
+    message.channel.bulkDelete(Number(args))
+    const {MessageEmbed}=require('discord.js')
+    const embed = new MessageEmbed()
+    .setTitle('Başarıyla mesajlar silindi.')
+    .setDiscription('Silinen mesaj sayısı: ' + args)
+    message.channel.send(embed).then(mesaj => {
+      setTimeout(function(){
+        mesaj.delete()
+      },2500);
+    })
+  }
+}
